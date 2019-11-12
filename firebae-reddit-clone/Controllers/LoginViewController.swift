18deece4692687eba3c56a723eff61b1e1a37f17
davidlawrencer <davidlrifkin+github@gmail.com>
2019-11-12 00:59:18 -0500

    Initial Commit

---
+++ firebae-reddit-clone/Controllers/LoginViewController.swift
@@ -0,0 +1,129 @@
+//
+//  ViewController.swift
+//  firebae-reddit-clone
+//
+//  Created by David Rifkin on 11/11/19.
+//  Copyright © 2019 David Rifkin. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    //MARK: UI
+    
+    lazy var logoLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.text = "Firebae"
+        label.font = UIFont(name: "Verdana-Bold", size: 60)
+        label.textColor = UIColor(red: 255/255, green: 86/255, blue: 0/255, alpha: 1.0)
+        label.backgroundColor = .clear
+        label.textAlignment = .center
+        return label
+    }()
+    
+    lazy var emailTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Enter Email"
+        textField.font = UIFont(name: "Verdana", size: 14)
+        textField.backgroundColor = .white
+        textField.borderStyle = .bezel
+        textField.addTarget(self, action: #selector(validateFields), for: .editingChanged)
+        return textField
+    }()
+    
+    lazy var passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Enter Password"
+        textField.isSecureTextEntry = true
+        textField.font = UIFont(name: "Verdana", size: 14)
+        textField.backgroundColor = .white
+        textField.borderStyle = .bezel
+        textField.addTarget(self, action: #selector(validateFields), for: .editingChanged)
+        return textField
+    }()
+    
+    lazy var loginButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Login", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = UIFont(name: "Verdana", size: 14)
+        button.backgroundColor = UIColor(red: 255/255, green: 67/255, blue: 0/255, alpha: 1)
+        button.layer.cornerRadius = 5
+        // button.addTarget(self, action: #selector(tryLogin), for: .touchUpInside)
+        button.isEnabled = false
+        return button
+    }()
+    
+    lazy var createAccountButton: UIButton = {
+        let button = UIButton(type: .system)
+        let attributedTitle = NSMutableAttributedString(string: "Dont have an account?  ", attributes: [NSAttributedString.Key.font: UIFont(name: "Verdana", size: 14)!, NSAttributedString.Key.foregroundColor: UIColor.white])
+        attributedTitle.append(NSAttributedString(string: "Sign Up", attributes: [NSAttributedString.Key.font: UIFont(name: "Verdana-Bold", size: 14)!, NSAttributedString.Key.foregroundColor:  UIColor(red: 17/255, green: 154/255, blue: 237/255, alpha: 1)]))
+        button.setAttributedTitle(attributedTitle, for: .normal)
+        button.addTarget(self, action: #selector(showSignUp), for: .touchUpInside)
+        return button
+    }()
+    
+    //MARK: Lifecycle methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor(red: 198/255, green: 198/255, blue: 198/255, alpha: 1)
+        setupSubViews()
+    }
+    
+    //MARK: Obj-C methods
+    @objc func validateFields() {
+        guard let email = emailTextField.text, !email.isEmpty && email.isValidEmail , let password =  passwordTextField.text, !password.isEmpty && password.isValidPassword else {
+            loginButton.backgroundColor = UIColor(red: 255/255, green: 67/255, blue: 0/255, alpha: 0.5)
+            loginButton.isEnabled = false
+            return
+        }
+        loginButton.isEnabled = true
+        loginButton.backgroundColor = UIColor(red: 255/255, green: 67/255, blue: 0/255, alpha: 1)
+    }
+    
+    @objc func showSignUp() {
+        let signupVC = SignUpViewController()
+        signupVC.modalPresentationStyle = .formSheet
+        present(signupVC, animated: true, completion: nil)
+    }
+    
+    //MARK: Private methods
+    private func setupSubViews() {
+        setupLogoLabel()
+        setupCreateAccountButton()
+        setupLoginStackView()
+    }
+    
+    private func setupLogoLabel() {
+        view.addSubview(logoLabel)
+
+        logoLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([logoLabel.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 60), logoLabel.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 16), logoLabel.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -16)])
+    }
+    
+    private func setupLoginStackView() {
+        let stackView = UIStackView(arrangedSubviews: [emailTextField, passwordTextField,loginButton])
+        stackView.axis = .vertical
+        stackView.spacing = 15
+        stackView.distribution = .fillEqually
+        self.view.addSubview(stackView)
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([stackView.bottomAnchor.constraint(equalTo: createAccountButton.topAnchor, constant: -50),
+                                     stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+                                     stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+                                     stackView.heightAnchor.constraint(equalToConstant: 130)])
+    }
+    
+    private func setupCreateAccountButton() {
+        view.addSubview(createAccountButton)
+        
+        createAccountButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([createAccountButton.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+          createAccountButton.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+          createAccountButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+          createAccountButton.heightAnchor.constraint(equalToConstant: 50)])
+    }
+}
---
+++ firebae-reddit-clone/Utilities/Firebase/FirebaseAuthService.swift
@@ -0,0 +1,14 @@
+//
+//  FirebaseAuthService.swift
+//  firebae-reddit-clone
+//
+//  Created by David Rifkin on 11/12/19.
+//  Copyright © 2019 David Rifkin. All rights reserved.
+//
+
+import Foundation
+
+class FirebaseAuthService {
+    static let manager = FirebaseAuthService()
+    
+}
